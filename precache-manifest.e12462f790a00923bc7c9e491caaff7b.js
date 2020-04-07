5519eae3d1e93e46a4be4d7e1e2cfa996338e0d3
Hussain Imtiaz <hussain.imtiaz99@gmail.com>
2020-04-08 01:50:53 +0500

    Updates

---
+++ precache-manifest.e12462f790a00923bc7c9e491caaff7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c460f1c3bbd2f4c017d2dc75d43331c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f9e17461cb579ec8b97",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eb732fa010ce26da3836",
+    "url": "/static/js/2.18f4e8ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.18f4e8ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f9e17461cb579ec8b97",
+    "url": "/static/js/main.c2a253a7.chunk.js"
+  },
+  {
+    "revision": "7141b81939104c717291",
+    "url": "/static/js/runtime-main.cd957f84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
